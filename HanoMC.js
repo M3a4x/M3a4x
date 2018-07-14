bc493b922995e51a45901235a9b3cda22a409ec5
M3a4x <38668201+M3a4x@users.noreply.github.com>
2018-07-14 19:22:23 +0300

    Update HanoMC.js

--- HanoMC.js
+++ HanoMC.js
@@ -17,9 +17,9 @@ client.user.setPresence({
      state: `في ڪڸ سن? هنآڪ شهر يجعڸني أإڪثر قرباً من ربي {شهر رمضآن}`,
     application_id: '467718532352835595',
      assets: {
-        small_image: `467727130147422208`,
+        small_image: `https://discordapp.com/channels/415630192342728714/467322179193798666/467727130147422208`,
         small_text: 'Play.HanoMC.Net',
-        large_image: `467727130147422208`,
+        large_image: `https://discordapp.com/channels/415630192342728714/467322179193798666/467727138754265088`,
         large_text: `Play.HanoMC.Net` }
 
   }
